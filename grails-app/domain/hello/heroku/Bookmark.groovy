dfdf88cf5b8581e104c3ed7f2ce00e1c5f117077
Yasuharu NAKANO <ynak@jggug.org>
2011-09-06 20:59:34 +0900

    added some fields to Bookmark

--- grails-app/domain/hello/heroku/Bookmark.groovy
+++ grails-app/domain/hello/heroku/Bookmark.groovy
@@ -2,6 +2,13 @@ package hello.heroku
 
 class Bookmark {
 
+    String title
+    String url
+    String remarks
+
     static constraints = {
+        title(blank:false)
+        url(blank:false)
+        remarks()
     }
 }
